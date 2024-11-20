6072c13a1a176a1ef1a3136d20be1377dd252335
Adam <adam.belshaw@hicomply.com>
2024-11-20 08:56:49 +0000

    back to 64x64

--- globals.h
+++ globals.h
@@ -1,5 +1,5 @@
 #define ATTR_BUFF 0xF800 // hard coded attribute buffer address
-#define MAP_SIZE 16 // 64x64 map
+#define MAP_SIZE 64 // 64x64 map
 #define VISIBLE_BLOCKS 11 // 11x11 displayed (must be odd number 3-11)
 #define VISIBLE_AREA (VISIBLE_BLOCKS * 2) // each block 2x2
 #define PLAYER_SQUARE (VISIBLE_BLOCKS - 1) // position of player on screen
